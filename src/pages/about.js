84ef8c7aa626264ae5ba400550f23bd0b625fd1e
Nur Kocar <nur.kocar@ltgplc.com>
2021-08-23 11:21:14 +0100

    added home and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function () {
+  return (
+    <main>
+      <h1>About my site</h1>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function () {
+  return (
+    <main>
+      <h1>Hi Nur</h1>
+    </main>
+  );
+}
