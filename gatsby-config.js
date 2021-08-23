fbed7335d8f5b2229e19d88664c0ab6f8bc36e10
Nur Kocar <nur.kocar@ltgplc.com>
2021-08-23 14:35:17 +0100

    add Seo component using React Hel,et

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,5 @@ module.exports = {
     image:
       'https://images.unsplash.com/photo-1505783638529-f3fdc62e1bb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
   },
+  plugins: ['gatsby-plugin-react-helmet'],
 };
---
+++ src/components/seo.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Helmet, helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+export function Seo(props) {
+  const data = useStaticQuery(graphql`
+    query GetSiteMetadata {
+      site {
+        siteMetadata {
+          title
+          siteUrl
+          image
+          description
+        }
+      }
+    }
+  `);
+
+  const defaults = data?.site?.sitMetadata;
+
+  const title = props.title || defaults.title;
+  const description = props.description || defaults.description;
+  const image = new URL(props.image || defaults.image, defaults.siteUrl);
+  const url = new URL(props.path || '/', defaults.siteUrl);
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+      {image && <meta name="image" content={image} />}
+
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={description} />
+      <meta property="og:description" content={url} />
+      {image && <meta name="og:image" content={image} />}
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+
+      <meta />
+    </Helmet>
+  );
+}
