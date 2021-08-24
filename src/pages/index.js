664e8c5a43f18a5c583b62924866099b724935a4
Nur Kocar <nur.kocar@ltgplc.com>
2021-08-24 13:56:53 +0100

    created post layout and used it for posts, add posts to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            description
+            date(fromNow: true)
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
       <h1>Hi Nur</h1>
       <Link to="/about">About this site</Link>
+
+      <h2>Check out my recent blog posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+            {post.frontmatter.date ? (
+              <small>posted {post.frontmatter.date}</small>
+            ) : (
+              'no date info'
+            )}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
