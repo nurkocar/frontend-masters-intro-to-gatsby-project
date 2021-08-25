91f18d227e1bcac63300a9752d494d9e00daec37
Nur Kocar <nur.kocar@ltgplc.com>
2021-08-25 16:10:30 +0100

    add gatsby image plugins and used them

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
