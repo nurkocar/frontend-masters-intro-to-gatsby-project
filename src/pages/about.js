4d427b90121ae5585a9e997d36fbb8c493d718a7
Nur Kocar <nur.kocar@ltgplc.com>
2021-09-17 12:47:22 +0100

    add page query to about page and an mdx file and used GatsbyImage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+
 import Layout from '../components/layout.js';
 
-export default function About() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function About({ data }) {
   return (
     <Layout
       title="About my site"
       description="More information about this site"
     >
+      <GatsbyImage image={getImage(data.file)} alt="a cocktail" />
       <h1>About my site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
